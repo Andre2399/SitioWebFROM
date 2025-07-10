98433bd6575377e6f7faca4bfb379a8bf8448972
Andre2399 <andrea.aguerrido@davinci.edu.ar>
2025-07-10 20:50:07 -0300

    diseño responsive elenco

---
+++ javaScript.js/elenco.js
@@ -0,0 +1,72 @@
+function mostrarInfo0(){
+var mostrarInfo =document.getElementById("boyd");
+mostrarInfo.style.display= "block";
+}
+function ocultarInfo0(){
+var ocultarInfo =document.getElementById("boyd");
+ocultarInfo.style.display="none";
+}
+function mostrarInfo1(){
+var mostrarInfo =document.getElementById("tabitha");
+mostrarInfo.style.display= "block";
+}
+function ocultarInfo1(){
+var ocultarInfo =document.getElementById("tabitha");
+ocultarInfo.style.display="none";
+}
+function mostrarInfo2(){
+var mostrarInfo =document.getElementById("jim");
+mostrarInfo.style.display= "block";
+}
+function ocultarInfo2(){
+var ocultarInfo =document.getElementById("jim");
+ocultarInfo.style.display="none";
+}
+function mostrarInfo3(){
+var mostrarInfo =document.getElementById("jade");
+mostrarInfo.style.display= "block";
+}
+function ocultarInfo3(){
+var ocultarInfo =document.getElementById("jade");
+ocultarInfo.style.display="none";
+}
+function mostrarInfo4(){
+var mostrarInfo =document.getElementById("dona");
+mostrarInfo.style.display= "block";
+}
+function ocultarInfo4(){
+var ocultarInfo =document.getElementById("dona");
+ocultarInfo.style.display="none";
+}
+function mostrarInfo5(){
+var mostrarInfo =document.getElementById("khatri");
+mostrarInfo.style.display= "block";
+}
+function ocultarInfo5(){
+var ocultarInfo =document.getElementById("khatri");
+ocultarInfo.style.display="none";
+}
+function mostrarInfo6(){
+var mostrarInfo =document.getElementById("victor");
+mostrarInfo.style.display= "block";
+}
+function ocultarInfo6(){
+var ocultarInfo =document.getElementById("victor");
+ocultarInfo.style.display="none";
+}
+function mostrarInfo7(){
+var mostrarInfo =document.getElementById("julie");
+mostrarInfo.style.display= "block";
+}
+function ocultarInfo7(){
+var ocultarInfo =document.getElementById("julie");
+ocultarInfo.style.display="none";
+}
+function mostrarInfo8(){
+var mostrarInfo =document.getElementById("ethan");
+mostrarInfo.style.display= "block";
+}
+function ocultarInfo8(){
+var ocultarInfo =document.getElementById("ethan");
+ocultarInfo.style.display="none";
+}

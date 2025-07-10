3510b4982b605e50e9b7962e4df4e3f603845b7a
Andre2399 <andrea.aguerrido@davinci.edu.ar>
2025-07-10 18:40:46 -0300

    diseño responsive

---
+++ javaScript.js/episodios.js
@@ -0,0 +1,24 @@
+function abrirListaT1 (){
+var abrirListaT1 =document.getElementById("temporada1");
+abrirListaT1.style.display= "block";
+}
+function cerrarListaT1(){
+var cerrarListaT1 =document.getElementById("temporada1");
+cerrarListaT1.style.display="none";
+}
+function abrirListaT2 (){
+var abrirListaT2 =document.getElementById("temporada2");
+abrirListaT2.style.display= "block";
+}
+function cerrarListaT2 (){
+var cerrarListaT2 =document.getElementById("temporada2");
+cerrarListaT2.style.display= "none";
+}
+function abrirListaT3 (){
+var abrirListaT3 =document.getElementById("temporada3");
+abrirListaT3.style.display= "block";
+}
+function cerrarListaT3 (){
+var cerrarListaT3 =document.getElementById("temporada3");
+cerrarListaT3.style.display= "none";
+}
